42c3ca93b0b39545270191ade90b2780c54d12a6
wujianbin <1195128500@qq.com>
2022-08-29 01:00:32 +0800

    pachong

--- hello.py
+++
@@ -1,4 +0,0 @@
-def chifan(a,b,c)
-    print(a,b,c)
-    
-    chifan("banana","apple","orange")
---
+++ practi.py
@@ -0,0 +1,15 @@
+#安装requests
+# pip install requests
+#pip 清华源
+
+import requests
+url = "https://www.baidu.com/s?wd=%E5%91%A8%E6%9D%B0%E4%BC%A6"
+
+dic = {"User-Agent":
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0"}
+
+resp = requests.get(url, headers=dic)  #处理一个小小的反爬，吧useragent放进去
+
+
+
+print(resp.text)   #拿取源代码
