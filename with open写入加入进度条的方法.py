eef7a9ef50bf8dd1d5039c559118c29474cfd5bb
verginbeln <95607450+verginbeln@users.noreply.github.com>
2022-10-27 15:45:49 +0800

    with open写入加入进度条的方法.py

---
+++ with open写入加入进度条的方法.py
@@ -0,0 +1,30 @@
+import requests
+from tqdm import tqdm
+
+def download_style(url: str, fname: str):
+    # 用流stream的方式获取url的数据
+    resp = requests.get(url, headers=headers, stream=True)
+    # 拿到文件的长度，并把total初始化为0
+    total = int(resp.headers.get('content-length', 0))
+    # 打开当前目录的fname文件(名字你来传入)
+    # 初始化tqdm，传入总数，文件名等数据，接着就是写入，更新等操作了
+    with open(fname, 'wb') as file, tqdm(
+        desc=fname,
+        total=total,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
+            
+            
+if __name__ == '__main__':
+    url = "https://www.bilibili.com/video/......."
+    headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+            "referer": url
+    }  
+    download_style(url: str, fname: str)
+            
